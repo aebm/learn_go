760cb61101b0cb7e706200e7898c2f9dfe8b9d6e
Alejandro E. Brito Monedero <abrito@alea-soluciones.com>
2019-11-26 15:42:05 +0100

    Refactor test

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -6,14 +6,6 @@ import (
 	"testing"
 )
 
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
 type CountdownOperationsSpy struct {
 	Calls []string
 }
@@ -31,6 +23,22 @@ func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
 }
 
 func TestCountdown(t *testing.T) {
+
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &CountdownOperationsSpy{})
+		got := buffer.String()
+		want := `3
+2
+1
+Go!`
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+	})
+
 	t.Run("sleep before every print", func(t *testing.T) {
 		spySleepPrinter := &CountdownOperationsSpy{}
 		Countdown(spySleepPrinter, spySleepPrinter)
@@ -51,22 +59,4 @@ func TestCountdown(t *testing.T) {
 		}
 	})
 
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
-
-	Countdown(buffer, spySleeper)
-
-	got := buffer.String()
-	want := `3
-2
-1
-Go!`
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-
-	if spySleeper.Calls != 4 {
-		t.Errorf("not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
-	}
 }
